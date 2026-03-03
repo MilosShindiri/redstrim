fbfe9b889d2f1c92619e333fada78fcc30444af1
Milos Torbica <m.torbica@shindiristudio.com>
2026-03-03 12:42:13 +0100

    feat: refactored settings page

--- src/components/Settings/ContentPanel.ts
+++ src/components/Settings/ContentPanel.ts
@@ -1,36 +1,19 @@
 import Blits from "@lightningjs/blits";
-import DeviceInfoSection from "./Sections/DeviceInfoSection";
-import AccountSection from "./Sections/AccountSection";
+import InfoSection from "./Sections/InfoSection";
 import ContactSection from "./Sections/ContactSection";
-import RestartSection from "./Sections/RestartSection";
-import ExitSection from "./Sections/ExitSection";
-import LogoutSection from "./Sections/LogoutSection";
 
-export default Blits.Component('ContentPanel', {
-  props: ['deviceInfo', 'section'],
+export default Blits.Component("ContentPanel", {
+  props: ["deviceInfo", "section"],
 
   components: {
-    DeviceInfoSection,
-    AccountSection,
+    InfoSection,
     ContactSection,
-    RestartSection,
-    ExitSection,
-    LogoutSection
   },
 
   template: `
     <Element>
-      <DeviceInfoSection :show="$section === 'Device information'" :deviceInfo="$deviceInfo" />
-    
-      <AccountSection :show="$section === 'Account information'" />
-    
+      <InfoSection :show="$section !== 'Contact us'" :deviceInfo="$deviceInfo" :section="$section" />
       <ContactSection :show="$section === 'Contact us'" :deviceInfo="$deviceInfo" />
-    
-      <RestartSection :show="$section === 'Restart App'" :deviceInfo="$deviceInfo" />
-    
-      <ExitSection :show="$section === 'Exit App'" :deviceInfo="$deviceInfo" />
-    
-      <LogoutSection :show="$section === 'Logout'" :deviceInfo="$deviceInfo" />
     </Element>
   `,
-})
+});
--- src/pages/Settings.ts
+++ src/pages/Settings.ts
@@ -1,15 +1,38 @@
-import Blits from '@lightningjs/blits'
-import GoBackButton from '../components/Settings/GoBackButton'
-import SideBar from '../components/Settings/SideBar'
-import { DeviceInfo, getStoredDeviceInfo } from '../utils/deviceInfo'
-import ContentPanel from '../components/Settings/ContentPanel'
+import Blits from "@lightningjs/blits";
+import GoBackButton from "../components/Settings/GoBackButton";
+import SideBar from "../components/Settings/SideBar";
+import { DeviceInfo, getStoredDeviceInfo } from "../utils/deviceInfo";
+import ContentPanel from "../components/Settings/ContentPanel";
 
+const accInfo = [
+  { profileName: "Loai" },
+  { accEmail: "example@email.com" },
+  { country: "AU / en-AU" },
+];
+const rrInfo = [
+  {
+    key: "Restart Redstrim",
+    value: "Restart the Redstrim application",
+  },
+];
+const exitInfo = [
+  {
+    key: "Exit",
+    value: "Are you sure you want to exit the application?",
+  },
+];
+const logoutInfo = [
+  {
+    key: "Logout your account",
+    value: "Logout of your Redstrim account on this device?",
+  },
+];
 
-export default Blits.Component('Settings', {
+export default Blits.Component("Settings", {
   components: {
     GoBackButton,
     SideBar,
-    ContentPanel
+    ContentPanel,
   },
 
   template: `
@@ -21,57 +44,88 @@ export default Blits.Component('Settings', {
     </Element>
   `,
 
-    state() {
-        return {
-            focused: 0,
-            selectedSection: 'Device information', // default
+  state() {
+    return {
+      focused: 0,
+      selectedSection: "Device information", // default
+      deviceInfo: [] as any,
+    };
+  },
 
-            deviceInfo: null as DeviceInfo | null
-        }
+  watch: {
+    focused(v: number) {
+      if (v === 0) {
+        this.$select("goback")?.$focus();
+      } else if (v === 1) {
+        this.$select("sidebar")?.$focus();
+      }
     },
 
-    watch: {
-        focused(v: number) {
-            if (v === 0) {
-                this.$select('goback')?.$focus()
-            } else if (v === 1) {
-                this.$select('sidebar')?.$focus()
-            }
-        },
+    selectedSection() {
+      console.log("asdf selected sect: ", this.selectedSection);
+      if (this.selectedSection === "Device information") {
+        const info = getStoredDeviceInfo(this.$storage);
+        this.deviceInfo = this.formatData(info);
+      } else if (this.selectedSection === "Account information") {
+        this.deviceInfo = this.formatData(accInfo);
+      } else if (this.selectedSection === "Restart App") {
+        this.deviceInfo = rrInfo;
+      } else if (this.selectedSection === "Exit App") {
+        this.deviceInfo = exitInfo;
+      } else if (this.selectedSection === "Logout") {
+        this.deviceInfo = logoutInfo;
+      }
+    },
   },
 
   hooks: {
-  init() {
-    this.$listen('sectionSelected', (section: string) => {
-      this.selectedSection = section
-    })
-  },
+    init() {
+      this.$listen("sectionSelected", (section: string) => {
+        this.selectedSection = section;
+      });
+    },
     ready() {
-      this.$select('goback')?.$focus()
+      this.$select("goback")?.$focus();
+      this.selectedSection === "Account information";
 
-      const info = getStoredDeviceInfo(this.$storage)
-      this.deviceInfo = info
-
-      // console.log('📦 SETTINGS DEVICE INFO →', info)
+      const info = getStoredDeviceInfo(this.$storage);
+      this.deviceInfo = this.formatData(info);
     },
     focus() {
-      this.$trigger('focused')
+      this.$trigger("focused");
     },
   },
 
-  input:{
+  methods: {
+    formatData(data) {
+      if (!data) return [];
+
+      //formatting for accInfo format
+      if (Array.isArray(data)) {
+        return data.map((obj) => {
+          console.log("asdf obj: ", Object.entries(obj));
+          const [key, value] = Object.entries(obj)[0];
+          return {
+            key,
+            value: value || "-",
+          };
+        });
+      }
+
+      //for deviceInfo format
+      return Object.entries(data).map(([key, value]) => ({
+        key,
+        value: value || "-",
+      }));
+    },
+  },
+
+  input: {
     down() {
-      this.focused = 1
+      this.focused = 1;
     },
     up() {
-      this.focused = 0
+      this.focused = 0;
     },
   },
-
-  // methods: {
-  //   onSectionSelected(section: string) {
-  //     this.selectedSection = section
-  //   }
-  // }
-
-})
+});
